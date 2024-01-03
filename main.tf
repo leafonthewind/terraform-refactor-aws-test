b48bf4b170153087bbaed5615082f3abfa3461b2
Matthew Edwards <matt@M1Air.lan>
2024-01-02 19:20:42 -0500

    move 1 resource to new name

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
-resource aws_s3_bucket s3_bucket {
+resource aws_s3_bucket very_specific_s3_bucket {
   bucket = "cantbelievethisnameisnottaken"
+}
+
+moved {
+  from = aws_s3_bucket.s3_bucket
+  to = aws_s3_bucket.very_specific_s3_bucket
 }
