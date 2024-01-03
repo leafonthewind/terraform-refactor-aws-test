607897bca19eecff6d4ace09690e24b7207b9741
Matthew Edwards <matt@M1Air.lan>
2024-01-02 19:23:56 -0500

    move two buckets into one big array

--- main.tf
+++ main.tf
@@ -1,7 +1,22 @@
-resource aws_s3_bucket very_specific_s3_bucket {
-  bucket = "cantbelievethisnameisnottaken"
+moved {
+  from = aws_s3_bucket.very_specific_s3_bucket
+  to = aws_s3_bucket.all_my_s3_buckets["very_specific_s3_bucket"]
 }
 
-resource aws_s3_bucket another_s3_bucket {
-  bucket = "cantbelievethisnameisnottaken100"
+moved {
+  from = aws_s3_bucket.another_s3_bucket
+  to = aws_s3_bucket.all_my_s3_buckets["another_s3_bucket"]
+}
+
+variable lots_of_buckets {
+  type = map(string)
+  default = {
+    "very_specific_s3_bucket": "cantbelievethisnameisnottaken",
+    "another_s3_bucket": "cantbelievethisnameisnottaken100"
+  }
+}
+
+resource aws_s3_bucket all_my_s3_buckets {
+  for_each = var.lots_of_buckets
+  bucket   = each.value
 }
