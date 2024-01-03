12840b96ab46294f23727e99c752f55e6ea55f60
Matthew Edwards <matt@M1Air.lan>
2024-01-02 19:21:47 -0500

    create second bucket

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource aws_s3_bucket very_specific_s3_bucket {
   bucket = "cantbelievethisnameisnottaken"
 }
 
-moved {
-  from = aws_s3_bucket.s3_bucket
-  to = aws_s3_bucket.very_specific_s3_bucket
+resource aws_s3_bucket another_s3_bucket {
+  bucket = "cantbelievethisnameisnottaken100"
 }
